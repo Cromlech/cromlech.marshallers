e49dab9ea7777198502fc63788bac9b4fcfd2668
Souheil CHELFOUH <trollfot@gmail.com>
2017-12-19 00:08:51 +0100

    Changing naming convention

---
+++ src/cromlech/marshallers/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from .interface import IMarshaller, Marshaller
+
+# builtin marshallers
+from ._json import JSONMarshaller
+from ._pickle import PickleMarshaller
+from ._marshal import MarshalMarshaller
+
+# Conditional marshallers : using wildcard for safety.
+from ._bson import *
+from ._msgpack import *
+from ._rapidjson import *
+from ._yaml import *
